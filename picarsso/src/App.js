f413c5726a08914769e575dd2d2ba55e8adffebb
Kornimate <kornimate@gmail.com>
2025-05-09 02:10:38 +0200

    fixed imports

--- picarsso/src/App.js
+++ picarsso/src/App.js
@@ -1,12 +1,13 @@
 import TopBar from './Components/TopBar';
 import './App.css';
+import Container from './Components/Container';
 
 function App() {
   return (
     <div className="App">
       <TopBar />
       <main className="App-header">
-        
+        <Container />
       </main>
     </div>
   );
